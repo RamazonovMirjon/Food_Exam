53a30b39bb53311a7b5e63e93eda92340eb37cee
Mirjon Ramazonov <mirxan2564@gmail.com>
2022-02-20 11:15:40 +0500

    food model yaratildi

---
+++ lib/model/food_model.dart
@@ -0,0 +1,11 @@
+class User {
+  int id;
+  String image;
+  String title;
+  String subtitle;
+  String filial;
+  double reyting;
+  int time;
+
+  User({required this.id, required this.title,required this.subtitle,required this.filial, required this.image, required this.reyting, required this.time});
+}
