73513180e3f6273589ab2782f8cebafa85e396f3
Mirjon Ramazonov <mirxan2564@gmail.com>
2022-02-20 23:56:07 +0500

    modell bilan datani birlashtirdim

---
+++ lib/data/final_card_data.dart
@@ -0,0 +1,24 @@
+import 'package:food/model/final_card_model.dart';
+
+class FinalCardData {
+  List<FinalCard> finalCardList = [
+    FinalCard(
+      id: 1,
+      image: "assets/images/bg.png",
+      title: "Cookie Sandwich",
+      subtitle: "Chinese",
+    ),
+    FinalCard(
+      id: 2,
+      image: "assets/images/bg-1.png",
+      title: "Chow Fun",
+      subtitle: "Chinese",
+    ),
+    FinalCard(
+      id: 3,
+      image: "assets/images/bg-2.png",
+      title: "Chow Fun",
+      subtitle: "Chinese",
+    ),
+  ];
+}
---
+++ lib/data/final_food_data.dart
@@ -0,0 +1,54 @@
+import 'package:food/model/final_foot_model.dart';
+
+class FinalFoodData {
+  List<FinalFood> finalFoodList = [
+    FinalFood(
+      id: 1,
+      title: 'Cookie Sandwich',
+      subtitle: "Shortbread, chocolate turtle cookies, and red velvet.",
+      sinf: "Chinese",
+      image: "assets/images/food.png",
+      narx: 7.45,
+    ),
+    FinalFood(
+      id: 2,
+      title: 'Cookie Sandwich',
+      subtitle: "Shortbread, chocolate turtle cookies, and red velvet.",
+      sinf: "Chinese",
+      image: "assets/images/food-1.png",
+      narx: 17.99,
+    ),
+    FinalFood(
+      id: 3,
+      title: 'Combo Sandwich',
+      subtitle: "Shortbread, chocolate turtle cookies, and red velvet.",
+      sinf: "Chinese",
+      image: "assets/images/food-2.png",
+      narx: 25.45,
+    ),
+     FinalFood(
+      id: 4,
+      title: 'Oyster Dish',
+      subtitle: "Shortbread, chocolate turtle cookies, and red velvet.",
+      sinf: "Chinese",
+      image: "assets/images/food-3.png",
+      narx: 5.95,
+    ),
+     FinalFood(
+      id: 5,
+      title: 'Oyster On Ice',
+      subtitle: "Shortbread, chocolate turtle cookies, and red velvet.",
+      sinf: "Chinese",
+      image: "assets/images/food-4.png",
+      narx: 25.45,
+    ),
+     FinalFood(
+      id: 6,
+      title: 'Fried Rice on Pot',
+      subtitle: "Shortbread, chocolate turtle cookies, and red velvet.",
+      sinf: "Chinese",
+      image: "assets/images/food-5.png",
+      narx: 25.45,
+    ),
+  ];
+}
